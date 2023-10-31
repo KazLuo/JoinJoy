c8b5ae3be4559f8690650f116e401740ecbcbe81
Kaz <ccc5211@gmail.com>
2023-10-31 22:45:46 +0800

    新增店家詳細資訊API

--- JoinJoy/Controllers/StoreController.cs
+++ JoinJoy/Controllers/StoreController.cs
@@ -15,7 +15,13 @@ namespace JoinJoy.Controllers
     [RoutePrefix("store")]
     public class StoreController : ApiController
     {
+
         private Context db = new Context();
+        /// <summary>
+        /// 店家資訊
+        /// </summary>
+        /// <param name="storeId">帶入stroeId產生店家資訊</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("store/{storeId}")]
         public IHttpActionResult GetStoreInfo(int storeId)
@@ -25,7 +31,7 @@ namespace JoinJoy.Controllers
             {
                 return Content(HttpStatusCode.NotFound, new { statusCode = HttpStatusCode.NotFound, status = false, message = "找不到店家資訊" });
             }
-            return Content(HttpStatusCode.OK, new { statusCode = HttpStatusCode.OK, status = true, message = "回傳店家資訊成功", data = new { store } });
+            return Content(HttpStatusCode.OK, new { statusCode = HttpStatusCode.OK, status = true, message = "回傳店家資訊成功", data = new {id=store.Id,memberId=store.MemberId,name=store.Name,plane=store.Address,tel=store.Phone,openTime=store.OpenTime,closeTime=store.CloseTime,introduce=store.Introduce,maxPeople=store.MaxPeople,price=store.Price,wifiTag=store.Wifi,teachTag=store.Teach,meal=store.Meal,mealout=store.Mealout,buffet=store.Buffet,HqTag=store.HqTag,popTag=store.PopTag  } });
         }
     }
 }
